4b79c10b7ad7068bc588ca6fbf5de7903e820cf0
David Navarro-Gonzalez <david.navarro@udl.cat>
2025-03-13 14:10:02 +0000

    version 1.0.6

--- inst/doc/vignette.R
+++ inst/doc/vignette.R
@@ -8,7 +8,7 @@ knitr::opts_chunk$set(
 #  ?acquihybrid
 
 ## ----echo=FALSE, results='asis'-----------------------------------------------
-knitr::kable(cbind(paste0("Respondent ", 1:10),head(siren::psymas, 10)), col.names = c("Respondent","I1 ","I2 ","I3 ","I4 ","I5 ","I6 ","I7 ","I8 ","I9 ","I10"))
+knitr::kable(cbind(paste0("responder", 1:10),head(siren::psymas, 10)), col.names = c("Responder","I1 ","I2 ","I3 ","I4 ","I5 ","I6 ","I7 ","I8 ","I9 ","I10"))
 
 ## ----echo=FALSE, results='asis'-----------------------------------------------
 knitr::kable(cbind(c("I1","I2","I3","I4","I5","I6","I7","I8","I9","I10"),c(-9,-9,0,0,0,9,0,0,9,0),c(0,0,-9,9,-9,0,9,-9,0,9)), col.names = c("Item","F1","F2"))
@@ -26,5 +26,5 @@ rownames(psymas_loadings) <- c("I1", "I2", "I6", "I9", "I3", "I4", "I5", "I7", "
 knitr::kable(psymas_loadings, col.names = c("Item", "F1", "F2", "ACQ"))
 
 ## ----echo=FALSE, results='asis'-----------------------------------------------
-knitr::kable(cbind(paste0("Respondent ", 1:10),head(round(psymas_pfactors,3), 10)), col.names = c("Respondent","F1", "F2", "ACQ"))
+knitr::kable(cbind(paste0("responder", 1:10),head(round(psymas_pfactors,3), 10)), col.names = c("Responder","F1", "F2", "ACQ"))
 
